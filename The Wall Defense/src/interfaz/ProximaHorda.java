427e3575c9ccd52135ec76a8d869bccca18bde91
Flor96 <flopyvic@hotmail.com>
2017-10-04 21:25:19 -0300

    personaje espadachin agregado

--- The Wall Defense/src/interfaz/ProximaHorda.java
+++ The Wall Defense/src/interfaz/ProximaHorda.java
@@ -1,11 +1,27 @@
 package interfaz;
 
+import java.awt.Color;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class ProximaHorda extends JPanel{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected JLabel puntaje;
+	
 	public ProximaHorda() {
 		
-		
+		this.setBorder(new LineBorder(new Color(0, 0, 0)));
+		agregarLabel();
+	}
+	
+	private void agregarLabel() {
+		puntaje=new JLabel("Puntaje: 1000");
+		this.add(puntaje);
 	}
 }
