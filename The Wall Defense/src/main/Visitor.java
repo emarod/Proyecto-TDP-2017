f80a96f024fed7b40aefa261be5212e8052a9f5f
flopy <flopy@192.168.56.1>
2017-10-12 15:56:46 -0300

    Setteando la compu de Flor.

--- The Wall Defense/src/main/Visitor.java
+++ The Wall Defense/src/main/Visitor.java
@@ -7,7 +7,7 @@ import obstaculo.*;
 
 public abstract  class Visitor {
 	   
-		public abstract boolean VisitRock(Rock r);
+	   public abstract boolean VisitRock(Rock r);
 	   public abstract boolean VisitWater(Water w);
 	   public abstract boolean visitPlayer(Jugador j);
 	   public abstract boolean visitDisparoPlayer(DisparoPlayer d);
