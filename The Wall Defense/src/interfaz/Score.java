13ae34e05c9ef3eabf805a8d8573088466f07e73
Flor <flopy@DESKTOP-FF13G4C>
2017-10-24 00:48:08 -0300

    agrego clase score e imagen

---
+++ The Wall Defense/src/interfaz/Score.java
@@ -0,0 +1,28 @@
+package interfaz;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Score extends JPanel {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected ProximaHorda horda;
+	
+	public Score(Escenario stage) {
+		this.setLayout(new BorderLayout());
+		horda=new ProximaHorda(stage);
+		stage.setHorda(horda);
+		armarPanel();
+		
+	}
+	
+	public void armarPanel() {
+		Icon bannerUp=new ImageIcon(this.getClass().getResource("/resources/static/banner/score2.png"));
+		this.add(new JLabel(bannerUp),BorderLayout.CENTER);
+		this.setBackground(Color.BLACK);
+		this.add(horda, BorderLayout.EAST);
+	}
+}
