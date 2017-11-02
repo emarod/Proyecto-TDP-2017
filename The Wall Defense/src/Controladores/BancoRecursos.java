500987937d07cec3e395f15d073e48986bc2b075
emarod <emrodk120@gmail.com>
2017-11-01 21:13:08 -0300

    musica activada

--- The Wall Defense/src/Controladores/BancoRecursos.java
+++ The Wall Defense/src/Controladores/BancoRecursos.java
@@ -11,19 +11,13 @@ public class BancoRecursos {
 	
 	public BancoRecursos() {
 		
-		//initialize TinySound
 		TinySound.init();
-		//load a sound and music
-		//note: you can also load with Files, URLs and InputStreams
-		//Music song = TinySound.loadMusic("/resources/sound/intro.wav");
-		//flecha = TinySound.loadSound("/resources/sound/laser4.wav");
-		//bola_fuego = TinySound.loadSound("/resources/sound/bola_fuego.wav");
 		Music song = TinySound.loadMusic("/resources/sound/theme_song.ogg");
 		flecha = TinySound.loadSound("/resources/sound/laser4.wav");
 		bola_fuego = TinySound.loadSound("/resources/sound/bola_fuego.wav");
 		//start playing the music on loop
-		//song.play(true);
-		//play the sound a few times in a loop
+		song.play(true);
+		
 	};
 	
 	public void playFlecha() {
