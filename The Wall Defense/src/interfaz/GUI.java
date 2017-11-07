aee47940e71a75f60762e70eab484dd623ce7994
Flor <flopy@DESKTOP-FF13G4C>
2017-11-06 23:52:13 -0300

    intento de pestañas

--- The Wall Defense/src/interfaz/GUI.java
+++ The Wall Defense/src/interfaz/GUI.java
@@ -3,6 +3,8 @@ package interfaz;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
@@ -23,6 +25,7 @@ public class GUI extends JFrame{
 	protected JPanel panelDerecho;
 	protected JPanel panelSuperior;
 	protected JPanel grafica;
+	protected JTabbedPane tabbedPane;
 	//Etiquetas en la interfaz
 	protected ProximaHorda horda;
 	protected Nivel level;
@@ -33,8 +36,8 @@ public class GUI extends JFrame{
 	public GUI() {	
 		super("The Wall Defense");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(0, 0, 1240, 550);
-		this.setResizable(true);
+		this.setBounds(0, 0, 1245, 570);
+		this.setResizable(false);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		
@@ -57,12 +60,45 @@ public class GUI extends JFrame{
 		
 		//Nivel
 		level=new Nivel(escenario);
+		
+		//Pestañas
+				tabbedPane = new JTabbedPane();
+				JPanel panel1 = new JPanel();
+				tabbedPane.addTab("Jugadores", panel1);
+				tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+				
+				JPanel panel2 = new JPanel();
+				tabbedPane.addTab("Power-ups", panel2);
+				tabbedPane.setMnemonicAt(1, KeyEvent.VK_1);
+
+				/*JComponent panel2 = makeTextPanel("Panel #2");
+				tabbedPane.addTab("Tab 2", icon, panel2,
+				                  "Does twice as much nothing");
+				tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+
+				JComponent panel3 = makeTextPanel("Panel #3");
+				tabbedPane.addTab("Tab 3", icon, panel3,
+				                  "Still does nothing");
+				tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+
+				JComponent panel4 = makeTextPanel(
+				        "Panel #4 (has a preferred size of 410 x 50).");
+				panel4.setPreferredSize(new Dimension(410, 50));
+				tabbedPane.addTab("Tab 4", icon, panel4,
+				                      "Does nothing at all");
+				tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);*/
 
 		//Panel Inferior
+		Icon bannerBottom=new ImageIcon(this.getClass().getResource("/resources/static/tienda/fondo.png"));
 		panelInferior= new JPanel();
 		panelInferior.setLayout(new BorderLayout());
-		panelInferior.setPreferredSize(new Dimension(10, 79));
+		panelInferior.setPreferredSize(new Dimension(10, 100));
+		panelInferior.setBackground(Color.BLACK);
+		panelInferior.add(new JLabel(bannerBottom));
+		panelInferior.add(tabbedPane);
 		getContentPane().add(panelInferior, BorderLayout.SOUTH);
+		
+		
 		
 		//Panel izquierdo
 		panelIzquierdo= new JPanel();
