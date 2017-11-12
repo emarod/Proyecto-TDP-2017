61aa80e481e5ccf100cec38059d5826ab7b2d195
Flor <flopy@DESKTOP-FF13G4C>
2017-11-12 14:55:59 -0300

    se actualizo clase score y se agrego la fuentede texto que usamos

--- The Wall Defense/src/interfaz/Score.java
+++ The Wall Defense/src/interfaz/Score.java
@@ -1,7 +1,11 @@
 package interfaz;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /*
  * Clase Score.
@@ -9,25 +13,37 @@ import java.awt.*;
  */
 
 public class Score extends JPanel {
-	
-	//Atributos locales.
+
+	// Atributos locales.
 	protected static final long serialVersionUID = 1L;
-	protected ProximaHorda horda;
-	
-	//Constructor.
+	protected JLabel puntaje;
+	protected int score;
+
+	// Constructor.
 	public Score(Escenario stage) {
 		this.setLayout(new BorderLayout());
-		horda=new ProximaHorda(stage);
-		stage.setHorda(horda);
-		armarPanel();
-		
-	}
-	
-	//Metodos locales.
-	public void armarPanel() {
-		//Icon bannerUp=new ImageIcon(this.getClass().getResource("/resources/static/banner/score2.png"));
-		//this.add(new JLabel(bannerUp),BorderLayout.CENTER);
 		this.setBackground(Color.BLACK);
-		this.add(horda, BorderLayout.EAST);
+		score = 0;
+		agregarLabel();
+
+	}
+
+	// Metodos locales.
+
+	private void agregarLabel() {
+		puntaje = new JLabel("Score 0000");
+		// puntaje.setSize(puntaje.getWidth(), this.getHeight());
+		// Recojo la fuente que se esta utilizando actualmente.
+		Font auxFont = new Font("ArcadeClassic", Font.CENTER_BASELINE, 50);
+		// Aplico la fuente actual, y al final le doy el tamaño del texto...
+		puntaje.setForeground(Color.WHITE);
+		puntaje.setFont(new Font(auxFont.getFontName(), auxFont.getStyle(), 50));
+		// puntaje.setSize(new Dimension(10,50));
+		this.add(puntaje, BorderLayout.CENTER);
+	}
+
+	public void setPuntaje(int p) {
+		score += p;
+		puntaje.setText("Score " + (score));
 	}
 }
