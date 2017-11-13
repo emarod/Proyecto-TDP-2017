518d31be615c18126b1f0c6cf4bca6b345bed13f
joaco1916 <joaquin_a_rodriguez@hotmail.com>
2017-11-13 01:03:06 -0300

    Agregado Diamante.

---
+++ The Wall Defense/src/tokens/Diamante.java
@@ -0,0 +1,17 @@
+package tokens;
+
+import javax.swing.ImageIcon;
+
+import mapa.Celda;
+
+public class Diamante extends Moneda {
+
+	public Diamante(Celda c, int prof) {
+		super(c);
+		grafico.setIcon(new ImageIcon(this.getClass().getResource("/resources/dinamic/diamante.gif")));
+		duracion = 10;
+		valor = 500;
+		activar();
+	}
+
+}
