5adedb81b86479f2e0bbabcd7b41c179eaa07a1d
joaco1916 <joaquin_a_rodriguez@hotmail.com>
2017-11-14 10:15:32 -0300

    Agregadas las clases LOBO, ESQUELETO_SUICIDA y cABALLERO_WW

---
+++ The Wall Defense/src/enemigo/Caballero.java
@@ -0,0 +1,79 @@
+package enemigo;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import mapa.Celda;
+
+public class Caballero extends Enemigo {
+
+	// Constructor.
+	public Caballero(Celda[] c) {
+		super(c);
+		puntaje = 100;
+		velocidad = 50;
+		vida = 8;
+		daño = 1;
+		graficos = new Icon[1];
+		graficos[0] = new ImageIcon(
+				this.getClass().getResource("/resources/dinamic/whitewalker_caballero_estatico.gif"));
+	}
+
+	// Metodos heredados.
+	@Override
+	public void atacar() {
+	}
+
+	@Override
+	public void setGrafico(JLabel grafico) {
+		ImageIcon imagen = new ImageIcon(
+				this.getClass().getResource("/resources/dinamic/whitewalker_caballero_estatico.gif"));
+		graph = 0;
+		grafico.setIcon(imagen);
+	}
+
+	public void setGraficos(Icon[] graficos, JLabel grafico) {
+
+	}
+
+	@Override
+	public int getPuntaje() {
+		return puntaje;
+	}
+
+	@Override
+	public Enemigo clone(Celda[] c) {
+		// Profundidad 1 predeterminada. Retorna una unidad de mismo tipo.
+		Enemigo clon = new WhiteWalker(c);
+		return clon;
+	}
+
+	@Override
+	public void playSound() {
+
+	}
+
+	@Override
+	public void destruir() {
+		super.destruir();
+		System.out.println("Destruir WhiteWalker");
+	}
+
+	@Override
+	public int getDaño() {
+		return daño;
+	}
+
+	@Override
+	public void guardarInicio() {
+		guardarEstado("WW_CABALLERO");
+	}
+
+	@Override
+	public void regresarInicio() {
+		reset("WW_CABALLERO");
+		careTaker.clearSavepoint();
+
+	}
+}
---
+++ The Wall Defense/src/jugador/Lobo.java
@@ -0,0 +1,75 @@
+package jugador;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import mapa.Celda;
+
+/*
+ * Clase Lobo.
+ * Clase que especifica las caracteristicas y comportamiento del jugador lobo.
+ */
+
+public class Lobo extends Jugador {
+
+	// Constructor.
+	public Lobo(Celda[] c) {
+		super(c);
+		vida = 3;
+		velocidad = 10;
+		costo = 20;
+		setGrafico(grafico);
+	}
+
+	// Metodos heredados.
+
+	@Override
+	public void atacar() {
+	}
+
+	@Override
+	public void setGrafico(JLabel grafico) {
+		ImageIcon imagen = new ImageIcon(
+				this.getClass().getResource("/resources/dinamic/personajes/lobo_estatico.gif"));
+		grafico.setIcon(imagen);
+	}
+
+	@Override
+	public Jugador clone(Celda[] c) {
+		// Profundidad 2 predeterminada. Retorna una unidad de mismo tipo.
+		Jugador clon = new Lobo(c);
+		return clon;
+	}
+
+	@Override
+	public void playSound() {
+
+	}
+
+	@Override
+	public void destruir() {
+		super.destruir();
+	}
+
+	@Override
+	public int getDaño() {
+		return daño;
+	}
+
+	@Override
+	public void mover() {
+	}
+
+	@Override
+	public void guardarInicio() {
+		guardarEstado("LOBO");
+	}
+
+	@Override
+	public void regresarInicio() {
+		reset("LOBO");
+		careTaker.clearSavepoint();
+
+	}
+
+}
